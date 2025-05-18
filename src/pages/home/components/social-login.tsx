dbdd14fde3f09b452197d83153ec274c4b00feae
eliscioter~ <attila433lehun@gmail.com>
2025-05-18 19:40:52 +0800

    feat: add file for social logins

--- src/pages/home/components/social-login.tsx
+++ src/pages/home/components/social-login.tsx
@@ -1,9 +1,9 @@
 export default function SocialLogins() {
-    return (
-        <div className="text-center mt-6">
-            <h1>Google</h1>
-            <h1>Facebook</h1>
-            <h1>GitHub</h1>
-        </div>
-    )
+	return (
+		<div className="text-center mt-6">
+			<h1>Google</h1>
+			<h1>Facebook</h1>
+			<h1>GitHub</h1>
+		</div>
+	);
 }
